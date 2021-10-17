ee95c91d8eb0176a201ae2890acbdfdcd16dfea8
kalyani joshi <kalyani121@gmail.com>
2021-10-17 07:12:48 -0400

    updated product list

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import ProductItem from '../ProductItem';
-// import { useStoreContext } from '../../utils/GlobalState';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { UPDATE_PRODUCTS } from '../../utils/actions';
@@ -9,10 +8,7 @@ import { QUERY_PRODUCTS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
 
-function ProductList() {
-  // TODO
-  // const [state, dispatch] = useStoreContext();
-  
+function ProductList() { 
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
 
